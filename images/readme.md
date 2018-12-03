4bb8d0c6f770322b0a1e5779eded2cc1b27d0d03
Juan Duque <jduque@mcclatchyservices.com>
2018-12-03 10:40:50 -0500

    Upload last version of the recognition to make it faster

--- images/readme.md
+++
@@ -1,1 +0,0 @@
-This folder is intended to temporaly save the video frame
--- recognizer.py
+++ recognizer.py
@@ -5,29 +5,28 @@ import os
 import matplotlib.pyplot as plt
 from CompareImage import CompareImage
 import progressbar
+import time
 
-
-
-templates = [file for file in glob.glob("images/*.jpg")]
-vid = cv2.VideoCapture('videos/ccrotation.mp4')
-
+cap = cv2.VideoCapture(0)
+t_end = time.time() + 3
 index = 0
-while (True):
-    ret, frame = vid.read()
-    if not ret:
-        break
+while time.time() < t_end:
+    ret, frame = cap.read()
     name = 'images/frame' + str(index) + '.jpg'
     cv2.imwrite(name, frame)
-    index += 1
+    index = index + 1
+cap.release()
+cv2.destroyAllWindows()
 
+templates = [file for file in glob.glob("template/*.jpg")]
+templates = templates[:15]
 videos = [file for file in glob.glob("images/frame*.jpg")]
 
 assertion = []
 
-
-bar = progressbar.ProgressBar(maxval=len(videos),widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar = progressbar.ProgressBar(maxval=len(videos),
+                              widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 bar.start()
-
 
 for idx, vid in enumerate(videos):
     scoring = []
@@ -35,33 +34,24 @@ for idx, vid in enumerate(videos):
         compare_image = CompareImage(file, vid)
         image_difference = compare_image.compare_image()
         scoring.append(image_difference)
-        # print("hello")
     bar.update(idx)
     assertion.append(np.mean(scoring))
 
 bar.finish()
-similarity_score = 1 - np.mean(assertion)
+similarity_score = np.mean(assertion)
 
 print(similarity_score)
 
-
-s_img = cv2.imread("images/frame0.jpg")
-l_img = 0
-if similarity_score >= 0.8:
+s_img = cv2.imread("template/frame0.jpg")
+img = 0
+if similarity_score <= 0.4:
     img = cv2.imread("approved.png")
-    l_img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 else:
     img = cv2.imread("rejected.png")
-    l_img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-
-x_offset = y_offset = 50
-l_img[y_offset:y_offset + s_img.shape[0], x_offset:x_offset + s_img.shape[1]] = s_img
-
 
 files = glob.glob('images/*')
 for f in files:
     os.remove(f)
 
-
-plt.imshow(l_img)
+plt.imshow(img)
 plt.show()
